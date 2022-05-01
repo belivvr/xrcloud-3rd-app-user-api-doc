1906f643ec20679e5e43b81a408e7f26a9f27675
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-05-01 21:57:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  <img src="https://avatars.githubusercontent.com/u/40684200?s=200&v=4" alt="Belivvr Logo" />
+</div>
+
+<div align="center">
+  <img src="https://img.shields.io/badge/nestjs-E0234E?style=for-the-badge&logo=nestjs&logoColor=white" alt="NestJS" />
+  <img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" alt="TypeScript" />
+  <img src="https://img.shields.io/badge/JWT-000000?style=for-the-badge&logo=JSON%20web%20tokens&logoColor=white" alt="JWT" />
+  <img src="https://img.shields.io/badge/Jest-C21325?style=for-the-badge&logo=jest&logoColor=white" alt="Jest" />
+  <img src="https://img.shields.io/badge/eslint-3A33D1?style=for-the-badge&logo=eslint&logoColor=white" alt="ESLint" />
+  <img src="https://img.shields.io/badge/prettier-1A2C34?style=for-the-badge&logo=prettier&logoColor=F7BA3E" alt="Prettier" />
+</div>
+
+## Description
+
+XRCloud 3rd app user's API Documents
+
+## What is XRCloud?
+
+XRCloud helps to make your custom Metaverse easily!  
+You can try to use XRCloud [here](http://developers.belivvr.com)
---
+++ docusaurus.config.js
@@ -0,0 +1,104 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'XRCloud 3rd App API Document',
+  tagline: 'Make your custom Metaverse easily!',
+  url: 'http://developers.belivvr.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Belivvr', // Usually your GitHub org/user name.
+  projectName: 'xrcloud-3rd-app-user-api-doc', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/belivvr/xrcloud-3rd-app-user-api-doc/tree/main/docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'XRCloud 3rd App API Document',
+        logo: {
+          alt: 'XRCloud 3rd App API Document Logo',
+          src: 'img/favicon.ico',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduce',
+            position: 'left',
+            label: 'Introduce',
+          },
+          {
+            type: 'doc',
+            docId: 'api',
+            position: 'left',
+            label: 'API',
+          },
+          {
+            href: 'https://github.com/belivvr',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Introduce',
+                to: '/docs/intro',
+              },
+              {
+                label: 'API',
+                to: '/docs/api',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: 'https://medium.com/belivvr',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/belivvr',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} BELIVVR, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
