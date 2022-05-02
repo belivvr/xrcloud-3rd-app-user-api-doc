d375b1c73c2dd5f2f129a904c8dc8bfac037445e
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-05-02 13:33:50 +0900

    Modify main page contents

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,68 +1,55 @@
 import React from 'react';
 import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+
 import styles from './styles.module.css';
-
-type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
 
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img className={styles.featureSvg} src="" alt="" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>
+                <Translate id="site.contents1.title">Easy Customize</Translate>
+              </h3> 
+              <p>
+                <Translate id="site.contents1.description">Easily customize the look and feel of the WebXR Metaverse with XRCloud!</Translate>
+              </p>
+            </div>
+          </div>
+
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img className={styles.featureSvg} src="" alt="" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>
+                <Translate id="site.contents2.title">Access with an Internet browser</Translate>
+              </h3>
+              <p>
+                <Translate id="site.contents2.description">Metaverse implemented with WebXR can be easily accessed from any device with only an Internet browser.</Translate>
+              </p>
+            </div>
+          </div>
+
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img className={styles.featureSvg} src="" alt="" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>
+                <Translate id="site.contents3.title">Free Contents</Translate>
+              </h3>
+              <p>
+                <Translate id="site.contents3.description">You can put anything that you can see in your Internet browser into the Metaverse world.</Translate>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
